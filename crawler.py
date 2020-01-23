188ade4577c16bd4e413caf8c67b205958fa6b68
Drew <abemery@clemson.edu>
2020-01-23 16:35:41 -0500

    was asked about making a webscraper in an interview yesterday so I thought I'd make some changes to this one

--- crawler.py
+++ crawler.py
@@ -2,41 +2,63 @@
 import json
 import sys
 import hashlib
-
-# installed libs
 import requests
 from bs4 import BeautifulSoup
+import threading
+import re
 
 # has list of links and scraping methods for pages / domains
 class crawler():
 
+    isAddress = r'https://www.foxnews.com'
+
     #  get the links on the domain's main page
     def __init__(self, domain):
         self.domain = domain
-        self.links = []
+        self.searched = set()
         self.scrapePage(self.domain)
 
     # appends all new links to self.links
     def scrapePage(self, address):
+        links = []
         r = requests.get(address)
+        print('QUERIED {}'.format(address))
         soup = BeautifulSoup(r.text, 'html.parser')
         theList = soup.find_all('a', href=True)
         for ref in theList:
-            if ref['href'] not in self.links:
-                self.links.append(ref['href'])
+            link = ref['href']
+            if re.match(self.isAddress, link):
+                if link not in self.searched:
+                    if link not in links:
+                        links.append(ref['href'])
 
-    # hash all of the links to make sure there aren't repeats
-    def makeHash(self, input):
-        m = hashlib.md5()
-        m.update(input)
-        m.digets()
-        return(m)
+    # thread the exploration of the generated set of links
+    def searchLinks(self, links):
+        self.searched |= set(links)
+        if len(links) > 0:
+            print("NUMBER OF THREADS:\t\t{}".format(len(links)))
+            threads = [threading.Thread(target=self.scrapePage, args=(link,)) for link in links]
+
+            for thread in threads:
+                thread.start()
+
+            for thread in threads:
+                thread.join()
+        else:
+            print('PASSED')
+
+        # print("SEARCHED:\t{}".format(len(self.searched)))
+
+        # if len(self.searched) > 1000:
+        self.fileWrite()
+
 
     # write all of the links to text file; deleting file will rebuild
     def fileWrite(self):
         with open('hrefs.txt', 'a') as f:
-            for link in self.links:
+            for link in self.searched:
                 f.write(link + '\n')
+            f.close()
 
 if __name__ == '__main__':
 
